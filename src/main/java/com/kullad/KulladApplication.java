852b72c9deea64eb7b5f8f7fcec311a382ecfef4
Prince Kumar Prasad <prince08833@gmail.com>
2024-12-05 22:41:15 +0530

    Initial Commit

---
+++ src/main/java/com/kullad/KulladApplication.java
@@ -0,0 +1,13 @@
+package com.kullad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KulladApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kullad/controllers/DemoController.java
@@ -0,0 +1,13 @@
+package com.kullad.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "Hello World";
+    }
+}
