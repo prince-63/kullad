e34ccdc0c412add1f647e90c2433d801a6c6a36a
Prince Kumar Prasad <prince08833@gmail.com>
2024-12-08 12:35:14 +0530

    modified some code in core application

--- api/src/main/java/com/kullad/KulladApplication.java
+++ api/src/main/java/com/kullad/KulladApplication.java
@@ -1,12 +1,34 @@
 package com.kullad;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {
+		"com.kullad.controllers",
+		"com.kullad.services",
+		"com.kullad.config",
+		"com.kullad.repositories"
+})
 public class KulladApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("APPLICATION_NAME", dotenv.get("APPLICATION_NAME"));
+		System.setProperty("PORT", dotenv.get("PORT"));
+		System.setProperty("DEFAULT_API_PATH", dotenv.get("DEFAULT_API_PATH"));
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+		System.setProperty("EMAIL_FROM", dotenv.get("EMAIL_FROM"));
+		System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
+		System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
+
 		SpringApplication.run(KulladApplication.class, args);
 	}
 
--- api/src/main/java/com/kullad/repositories/OrderRepository.java
+++ api/src/main/java/com/kullad/repositories/OrderRepository.java
@@ -1,9 +1,9 @@
 package com.kullad.repositories;
 
-import com.kullad.models.Order;
+import com.kullad.models.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 }
