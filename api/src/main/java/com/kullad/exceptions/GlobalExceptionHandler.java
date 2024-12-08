77cebbc68c87a535390966ae6060a3088688c62e
Prince Kumar Prasad <prince08833@gmail.com>
2024-12-08 12:29:08 +0530

    implimented custom exception handler and global exception hanler

---
+++ api/src/main/java/com/kullad/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.kullad.exceptions;
+
+import com.kullad.dto.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGlobalException(
+            Exception ex, WebRequest request
+    ) {
+        ErrorResponseDTO errorResponseDTO = ErrorResponseDTO.builder()
+                .apiPath(request.getDescription(false))
+                .errorCode(HttpStatus.INTERNAL_SERVER_ERROR)
+                .errorTimestamp(LocalDateTime.now())
+                .errorMessage(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(
+                errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorResponseDTO errorResponseDTO = ErrorResponseDTO.builder()
+                .apiPath(request.getDescription(false))
+                .errorTimestamp(LocalDateTime.now())
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.NOT_FOUND)
+                .build();
+
+        return new ResponseEntity<>(
+                errorResponseDTO, HttpStatus.NOT_FOUND
+        );
+    }
+}
