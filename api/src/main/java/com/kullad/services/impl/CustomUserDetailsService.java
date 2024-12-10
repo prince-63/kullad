bd307c33d567e1beb24f094ba5eac4c977435dc1
Prince Kumar Prasad <prince08833@gmail.com>
2024-12-10 08:48:26 +0530

    Implemented custom user details service.

---
+++ api/src/main/java/com/kullad/services/impl/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.kullad.services.impl;
+
+import com.kullad.models.User;
+import com.kullad.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User details not found for the user: " + username));
+        List<GrantedAuthority> authorities = List
+                .of(new SimpleGrantedAuthority(user.getRole().toString()));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
