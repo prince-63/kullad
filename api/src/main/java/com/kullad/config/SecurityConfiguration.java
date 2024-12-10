3ef4b8a0e8c493a6b7f82ad0773f4520bf8b78f7
Prince Kumar Prasad <prince08833@gmail.com>
2024-12-10 08:50:34 +0530

    Modified and added session management configuration and http basic login and exception handler

--- api/src/main/java/com/kullad/config/SecurityConfiguration.java
+++ api/src/main/java/com/kullad/config/SecurityConfiguration.java
@@ -1,32 +1,45 @@
 package com.kullad.config;
 
+import com.kullad.exceptions.CustomAccessDeniedHandler;
+import com.kullad.exceptions.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
+                        .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((req) ->
-                        req.requestMatchers("/files/**").permitAll()
-                                .requestMatchers("/demo").permitAll()
-                                .requestMatchers("/image/**").permitAll()
-                                .requestMatchers("/product/**").permitAll()
+                        req.requestMatchers("/files/**").authenticated()
+                                .requestMatchers("/demo", "/auth/**").permitAll()
+                                .requestMatchers("/product/**").hasRole("ADMIN")
                 )
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()))
+                .exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()))
                 .logout(Customizer.withDefaults());
         return http.build();
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
 }
